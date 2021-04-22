d873dfef333b640ad3f2c88855ccd7ed04ffec1b
takky94 <infourbh@gmail.com>
2021-04-22 12:07:51 +0900

    とりあえずWPテーマ雛形

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+  <?php wp_head(); // 削除禁止 ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <div id="container">
+    <!-- header -->
+    <header class="header">
+    </header>
+    <!-- // header -->
+    <!-- main -->
+    <main class="main">
+      <!-- メインビジュアル -->
+      <section class="hero">
+        <div class="news">
+        </div>
+      </section>
+      <!-- // メインビジュアル -->
+      <!-- 導入企業様 -->
+      <section class="case">
+      </section>
+      <!-- // 導入企業様 -->
+      <!-- // 手のひらから宇宙まで -->
+      <section class="lead"></section>
+      <!-- // 手のひらから宇宙まで -->
+      <!-- リンクリスト -->
+      <nav class="menu"></nav>
+      <!-- // リンクリスト -->
+      <!-- 型製品 -->
+      <section class="mold"></section>
+      <!-- // 型製品 -->
+      <!-- 砂型鋳造用資材・原材料 -->
+      <section class="sand-casting"></section>
+      <!-- // 砂型鋳造用資材・原材料 -->
+      <!-- 精密鋳造用材料 -->
+      <section class="investment-casting"></section>
+      <!-- // 精密鋳造用材料 -->
+      <!-- ジュエリーキャスト用副資材 -->
+      <section class="jewelry"></section>
+      <!-- // ジュエリーキャスト用副資材 -->
+      <!-- 新たな取り組み -->
+      <section class="new-field"></section>
+      <!-- // 新たな取り組み -->
+    </main>
+    <!-- // main -->
+    <footer class="footer">
+    </footer>
+  </div>
+</body>
+
+</html>
