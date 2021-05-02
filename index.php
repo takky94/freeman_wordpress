40b5b49f0818a67c436bf68fb83b412a2f029306
takky94 <infourbh@gmail.com>
2021-05-02 16:44:44 +0900

    ヒーロー部分のレスポンシブ化

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
   <!-- メインビジュアル -->
   <section class="hero">
     <h2 class="hero__heading font-eb">
-          <span>To Be </span><span class="c-main">P</span><span>OSSIBLE</span>
-          <br />
-          <span class="small">モノづくりに無限の可能性を</span>
+          <span>To be </span><span class="c-main">P</span><span>OSSIBLE</span>
+          <span class="small font-gothic">モノづくりに無限の可能性を</span>
         </h2>
     <div class="hero__slide">
       <picture>
@@ -46,7 +45,7 @@
     </div>
   </section>
   <!-- // 導入企業様 -->
-  <!-- // 手のひらから宇宙まで -->
+  <!-- 手のひらから宇宙まで -->
   <section class="lead">
     <div class="center">
       <h2 class="lead__heading center dev"><span class="c-main">手</span>のひらから<span class="c-main">宇</span>宙まで</h2>
@@ -185,96 +184,101 @@
     </div>
   </nav>
   <!-- // リンクリスト -->
-  <!-- 型製品 -->
-  <section class="mold" id="mold">
-    <div class="container dev">
-      <!-- detail -->
-      <div class="detail flex">
-        <div class="detail__text">
-          <h2 class="detail__text--headingEn font-gothic dev">M<span class="decoration c-main-bg">O</span>LD</h2>
-          <p class="detail__text--headingJp bold dev">型製品</p>
-          <p class="detail__text--headingSub dev">無限に広がる表現力</p>
-          <p
-            class="detail__text--description dev">デザインから試作開発、量産にいたる全ての領域で、モノづくりに必要な技術や製品を提案し、イメージの具体化や納期改善、コストダウンに貢献します。</p>
+  <div class="section__background">
+    <!-- 型製品 -->
+    <section class="mold" id="mold">
+      <div class="container dev">
+        <!-- detail -->
+        <div class="detail flex">
+          <div class="detail__text">
+            <h2 class="detail__text--headingEn font-gothic dev">M<span class="decoration c-main-bg">O</span>LD</h2>
+            <p class="detail__text--headingJp bold dev">型製品</p>
+            <p class="detail__text--headingSub dev">無限に広がる表現力</p>
+            <p
+              class="detail__text--description dev">デザインから試作開発、量産にいたる全ての領域で、モノづくりに必要な技術や製品を提案し、イメージの具体化や納期改善、コストダウンに貢献します。</p>
+          </div>
+          <div class="detail__image">
+            <picture>
+              <img src="<?= get_template_directory_uri(); ?>/images/top/mold.png" alt="" />
+            </picture>
+          </div>
         </div>
-        <div class="detail__image">
-          <picture>
-            <img src="<?= get_template_directory_uri(); ?>/images/top/mold.png" alt="" />
-          </picture>
-        </div>
+        <!-- // detail -->
       </div>
-      <!-- // detail -->
-    </div>
-  </section>
-  <!-- // 型製品 -->
-  <!-- 砂型鋳造用資材・原材料 -->
-  <section class="sand-casting" id="sand-casting">
-    <div class="container dev">
-      <!-- detail -->
-      <div class="detail detail-reverse flex">
-        <div class="detail__text">
-          <h2
-            class="detail__text--headingEn font-gothic dev">S<span class="decoration c-main-bg">A</span>ND CASTING</h2>
-          <p class="detail__text--headingJp bold dev">砂型鋳造用資材・原材料</p>
-          <p class="detail__text--headingSub dev">ヨーロッパクオリティで<br />高品質な鋳物作りを</p>
-          <p
-            class="detail__text--description dev">本場ヨーロッパの鋳物づくりを支えるLANIKセラミックフォームフィルターを筆頭に、海外から独自ルートで仕入れる最良の鋳造用資材を多岐にわたってお取り扱いしております。</p>
+    </section>
+    <!-- // 型製品 -->
+    <!-- 砂型鋳造用資材・原材料 -->
+    <section class="sand-casting" id="sand-casting">
+      <div class="container dev">
+        <!-- detail -->
+        <div class="detail detail-reverse flex">
+          <div class="detail__text">
+            <h2
+              class="detail__text--headingEn font-gothic dev">S<span class="decoration c-main-bg">A</span>ND CASTING</h2>
+            <p class="detail__text--headingJp bold dev">砂型鋳造用資材・原材料</p>
+            <p class="detail__text--headingSub dev">ヨーロッパクオリティで<br />高品質な鋳物作りを</p>
+            <p
+              class="detail__text--description dev">本場ヨーロッパの鋳物づくりを支えるLANIKセラミックフォームフィルターを筆頭に、海外から独自ルートで仕入れる最良の鋳造用資材を多岐にわたってお取り扱いしております。</p>
+          </div>
+          <div class="detail__image">
+            <picture>
+              <img src="<?= get_template_directory_uri(); ?>/images/top/sand-casting.png" alt="" />
+            </picture>
+          </div>
         </div>
-        <div class="detail__image">
-          <picture>
-            <img src="<?= get_template_directory_uri(); ?>/images/top/sand-casting.png" alt="" />
-          </picture>
-        </div>
+        <!-- // detail -->
       </div>
-      <!-- // detail -->
-    </div>
-  </section>
-  <!-- // 砂型鋳造用資材・原材料 -->
-  <!-- 精密鋳造用材料 -->
-  <section class="investment-casting" id="investment-casting">
-    <div class="container dev">
-      <!-- detail -->
-      <div class="detail flex">
-        <div class="detail__text">
-          <h2
-            class="detail__text--headingEn font-gothic dev">INVE<span class="decoration c-main-bg">S</span>TMENT CASTING</h2>
-          <p class="detail__text--headingJp bold dev">精密鋳造用材料</p>
-          <p class="detail__text--headingSub dev">高付加価値な製品<br />ラインナップと技術サービス</p>
-          <p
-            class="detail__text--description dev">国内外より厳選した、精密鋳造プロセスに欠かせない各種材料を取り揃え、最適なソリューションを提供。航空機・自動車・産業用ガスタービン・一般産業機械などの基幹産業を支えています。</p>
+    </section>
+    <!-- // 砂型鋳造用資材・原材料 -->
+  </div>
+  <div class="section__background">
+
+    <!-- 精密鋳造用材料 -->
+    <section class="investment-casting" id="investment-casting">
+      <div class="container dev">
+        <!-- detail -->
+        <div class="detail flex">
+          <div class="detail__text">
+            <h2
+              class="detail__text--headingEn font-gothic dev">INVE<span class="decoration c-main-bg">S</span>TMENT CASTING</h2>
+            <p class="detail__text--headingJp bold dev">精密鋳造用材料</p>
+            <p class="detail__text--headingSub dev">高付加価値な製品<br />ラインナップと技術サービス</p>
+            <p
+              class="detail__text--description dev">国内外より厳選した、精密鋳造プロセスに欠かせない各種材料を取り揃え、最適なソリューションを提供。航空機・自動車・産業用ガスタービン・一般産業機械などの基幹産業を支えています。</p>
+          </div>
+          <div class="detail__image">
+            <picture>
+              <img src="<?= get_template_directory_uri(); ?>/images/top/investment-casting.png" alt="" />
+            </picture>
+          </div>
         </div>
-        <div class="detail__image">
-          <picture>
-            <img src="<?= get_template_directory_uri(); ?>/images/top/investment-casting.png" alt="" />
-          </picture>
-        </div>
+        <!-- // detail -->
       </div>
-      <!-- // detail -->
-    </div>
-  </section>
-  <!-- // 精密鋳造用材料 -->
-  <!-- ジュエリーキャスト用副資材 -->
-  <section class="jewelry" id="jewelry">
-    <div class="container dev">
-      <!-- detail -->
-      <div class="detail detail-reverse flex">
-        <div class="detail__text">
-          <h2 class="detail__text--headingEn font-gothic dev">JE<span class="decoration c-main-bg">W</span>ELRY</h2>
-          <p class="detail__text--headingJp bold dev">ジュエリーキャスト用副資材</p>
-          <p class="detail__text--headingSub dev">業界のスタンダードとして</p>
-          <p
-            class="detail__text--description dev">ジュエリー業界で最も著名かつ実績のある、FreemanワックスおよびR&R埋没材の豊富なラインナップの中から、ご要望にあわせて提案します。その他関連材、設備もお任せください。</p>
+    </section>
+    <!-- // 精密鋳造用材料 -->
+    <!-- ジュエリーキャスト用副資材 -->
+    <section class="jewelry" id="jewelry">
+      <div class="container dev">
+        <!-- detail -->
+        <div class="detail detail-reverse flex">
+          <div class="detail__text">
+            <h2 class="detail__text--headingEn font-gothic dev">JE<span class="decoration c-main-bg">W</span>ELRY</h2>
+            <p class="detail__text--headingJp bold dev">ジュエリーキャスト用副資材</p>
+            <p class="detail__text--headingSub dev">業界のスタンダードとして</p>
+            <p
+              class="detail__text--description dev">ジュエリー業界で最も著名かつ実績のある、FreemanワックスおよびR&R埋没材の豊富なラインナップの中から、ご要望にあわせて提案します。その他関連材、設備もお任せください。</p>
+          </div>
+          <div class="detail__image">
+            <picture>
+              <img src="<?= get_template_directory_uri(); ?>/images/top/jewelry.png" alt="" />
+            </picture>
+          </div>
         </div>
-        <div class="detail__image">
-          <picture>
-            <img src="<?= get_template_directory_uri(); ?>/images/top/jewelry.png" alt="" />
-          </picture>
-        </div>
+        <!-- // detail -->
       </div>
-      <!-- // detail -->
-    </div>
-  </section>
-  <!-- // ジュエリーキャスト用副資材 -->
+    </section>
+    <!-- // ジュエリーキャスト用副資材 -->
+  </div>
   <!-- 新たな取り組み -->
   <section class="new-field" id="new-field">
     <div class="container dev">
