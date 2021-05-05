0b30eea7ec3bd68768376530246e8a4bc0f2cece
takky94 <infourbh@gmail.com>
2021-05-05 10:34:24 +0900

    ぱんくずCSS

--- category.php
+++ category.php
@@ -6,9 +6,11 @@
   $category_slug = get_query_var('category_name');
 ?>
 <!--カテゴリページ-->
-<main class="main <?php echo $category_slug; ?>" role="main">
-  <?php get_template_part('/parts/header/page-header'); ?>
-  <?php get_template_part('/parts/category-top/'.$category_slug); ?>
+<main id="main" class="main <?php echo $category_slug; ?>" role="main">
+  <article id="article" <?php post_class(); ?>>
+    <?php get_template_part('/parts/header/page-header'); ?>
+    <?php get_template_part('/parts/category-top/'.$category_slug); ?>
+  </article>
 </main>
 
 <?php get_footer(); ?>
