57ce0bb7d7250b088c57ebda5e98459cb72f394c
takky94 <infourbh@gmail.com>
2021-05-13 15:19:40 +0900

    FV修正

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
 {
-  const _w = window,
-    _d = document;
-
-  _w.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("DOMContentLoaded", function () {
     hamburgerMenu();
     accordionMenu();
   });
 
   // ハンバーガー
   function hamburgerMenu() {
-    const button = _d.querySelector(".js-menu");
-    const target = _d.querySelector(".js-menuContent");
+    const button = document.querySelector(".js-menu");
+    const target = document.querySelector(".js-menuContent");
 
     button.addEventListener("click", function (e) {
       e.preventDefault();
       button.classList.toggle("js-active");
       target.classList.toggle("js-active");
-      _d.body.classList.toggle("js-fixed");
+      document.body.classList.toggle("js-fixed");
     });
   }
 
   // アコーディオン
   function accordionMenu() {
-    const buttons = _d.querySelectorAll(".js-accordion");
-    const targets = _d.querySelectorAll(".js-accordionContent");
+    const buttons = document.querySelectorAll(".js-accordion");
+    const targets = document.querySelectorAll(".js-accordionContent");
 
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].addEventListener("click", function (e) {
