6877ad618eaf4bd37d16e196e4dcc80742f2e769
takky94 <infourbh@gmail.com>
2021-05-14 13:13:51 +0900

    フッターメニュー文字

--- footer.php
+++ footer.php
@@ -38,72 +38,75 @@
         alt="株式会社FREEMAN ロゴ" />
       <div class="sitemap__menu">
         <!-- sitemap__block -->
-        <div class="sitemap__block">
+        <div class="sitemap__block grow-1">
           <div class="sitemap__block--title">試作・型</div>
           <div class="sitemap__block--titleSub c-main font-robot">MOLD</div>
           <ul class="sitemap__block--menu col">
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
+            <li><a href="#">デザイン吐出</a></li>
+            <li><a href="#">デザイン切削</a></li>
+            <li><a href="#">試作注型樹脂</a></li>
+            <li><a href="#">試作型材</a></li>
+            <li><a href="#">試作シリコン</a></li>
+            <li><a href="#">量産インバー</a></li>
+            <li><a href="#">量産砂型鋳造</a></li>
+            <li><a href="#">量産精密鋳造</a></li>
+            <li><a href="#">量産ジュエリー</a></li>
+            <li><a href="#">特殊</a></li>
           </ul>
         </div>
         <!-- // sitemap__block -->
         <!-- sitemap__block -->
-        <div class="sitemap__block">
+        <div class="sitemap__block shrink">
           <div class="sitemap__block--title">精密鋳造</div>
           <div class="sitemap__block--titleSub c-main font-robot">INVESTMENT CASTING</div>
           <ul class="sitemap__block--menu">
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
+            <li><a href="#">WAX</a></li>
+            <li><a href="#">理散財・洗浄剤</a></li>
+            <li><a href="#">バインダー</a></li>
+            <li><a href="#">砂</a></li>
+            <li><a href="#">その他</a></li>
           </ul>
         </div>
         <!-- // sitemap__block -->
         <!-- sitemap__block -->
-        <div class="sitemap__block">
+        <div class="sitemap__block shrink">
           <div class="sitemap__block--title">砂型鋳造</div>
           <div class="sitemap__block--titleSub c-main font-robot">SAND CASTING</div>
           <ul class="sitemap__block--menu">
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
+            <li><a href="#">鋳造用フィルター</a></li>
+            <li><a href="#">スリープ</a></li>
+            <li><a href="#">方案用ゲート</a></li>
+            <li><a href="#">非鉄用塗型</a></li>
+            <li><a href="#">各種対火物</a></li>
+            <li><a href="#">アルミ原材料</a></li>
           </ul>
         </div>
         <!-- // sitemap__block -->
         <!-- sitemap__block -->
-        <div class="sitemap__block">
+        <div class="sitemap__block shrink">
           <div class="sitemap__block--title">ジュエリー</div>
           <div class="sitemap__block--titleSub c-main font-robot">JUWELRY</div>
           <ul class="sitemap__block--menu">
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
+            <li><a href="#">インジェクションWAX</a></li>
+            <li><a href="#">切削WAX</a></li>
+            <li><a href="#">埋没材</a></li>
+            <li><a href="#">シリコン</a></li>
+            <li><a href="#">ツールマット</a></li>
           </ul>
         </div>
         <!-- // sitemap__block -->
         <!-- sitemap__block -->
-        <div class="sitemap__block">
-          <div class="sitemap__block--title">試作</div>
+        <div class="sitemap__block grow-2">
+          <div class="sitemap__block--title">新たな取り組み</div>
           <div class="sitemap__block--titleSub c-main font-robot">MOLD</div>
-          <ul class="sitemap__block--menu">
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">MENU</a></li>
+          <ul class="sitemap__block--menu col">
+            <li><a href="#">消臭剤</a></li>
+            <li><a href="#">ミネラルキャスティング</a></li>
+            <li><a href="#">CO2洗浄システム</a></li>
+            <li><a href="#">ホットメルト</a></li>
+            <li><a href="#">暑さ対策</a></li>
+            <li><a href="#">電動アシスト台車</a></li>
+            <li><a href="#">ベアリングセンサーシステム</a></li>
           </ul>
         </div>
         <!-- // sitemap__block -->
