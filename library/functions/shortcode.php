f07e279a2cdfc5511c8b21ab46c31f4c4c2dbfcc
takky94 <infourbh@gmail.com>
2021-05-16 14:28:10 +0900

    TOP文字サイズ調整

--- library/functions/shortcode.php
+++ library/functions/shortcode.php
@@ -38,7 +38,7 @@ if (!function_exists('fm_get_product')){
 
     while ($wp_query -> have_posts()){
       $wp_query -> the_post();
-      $str .= '<li><a href="'.get_the_permalink().'" class="post-card-product"><div class="thumbail"><p class="post-thumbnail"><img src="'.fm_default_thumb('thumb-600').'" alt="" loading="lazy" /></p></div><div class="content"><p class="title">'.get_the_title().'</p><p class="more font-robot c-main">MORE</p></div></a></li>';
+      $str .= '<li><a href="'.get_the_permalink().'" class="post-card-product"><div class="thumbail"><p class="post-thumbnail"><img src="'.fm_default_thumb('thumb-600').'" alt="" loading="lazy" /></p></div><div class="content"><p class="title">'.get_the_title().'</p><p class="more font-robot c-main"><span>MORE</span></p></div></a></li>';
     }
 
     $str .= '</ul></div>';
