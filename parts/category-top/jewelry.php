22dc40dcd29ee17d43a78f485b0b04077f243ed1
takky94 <infourbh@gmail.com>
2021-05-23 18:21:04 +0900

    ジュエリーカテゴリページ(表以外)

--- parts/category-top/jewelry.php
+++ parts/category-top/jewelry.php
@@ -8,8 +8,151 @@
   </div>
   <div class="category-lead__image">
     <picture>
-      <img src="<?= get_template_directory_uri(); ?>/images/top/new-field.png" alt="" <?php fm_lazyload(); ?> />
+      <img src="<?= get_template_directory_uri(); ?>/images/top/jewelry.png" alt="" <?php fm_lazyload(); ?> />
     </picture>
   </div>
 </div>
 <!-- // category-lead -->
+<!-- category-children -->
+<div class="category-children left-text">
+  <div class="category-children__top">
+    <div class="category-children__top--text">
+      <h3 class="label">インジェクションWAX</h3>
+      <p class="title">優れた流動性と再現性</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/jewelry/children-1.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">INJECTION WAX</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children right-text">
+  <div class="category-children__top">
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/jewelry/children-2.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+    <div class="category-children__top--text">
+      <h3 class="label">カービングWAX</h3>
+      <p class="title">あらゆるジュエリー<br />パターンの作製が可能</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">CARVING WAX</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children left-text">
+  <div class="category-children__top">
+    <div class="category-children__top--text">
+      <h3 class="label">埋没材</h3>
+      <p class="title">高品質な<br />ジュエリーキャスト</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/jewelry/children-3.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">EMBEDDED MATERIAL</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children right-text">
+  <div class="category-children__top">
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/jewelry/children-4.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+    <div class="category-children__top--text">
+      <h3 class="label">シリコーン</h3>
+      <p class="title">デザインに忠実・高精度<br />に再現可能</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">CARVING WAX</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children left-text">
+  <div class="category-children__top">
+    <div class="category-children__top--text">
+      <h3 class="label">MAtt Products</h3>
+      <p class="title">プロセスの最適化と<br />品質向上</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/jewelry/children-5.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">MATT PRODUCTS</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-related -->
+<div class="category-related">
+  <div class="products">
+    <h4>試作・型材料の関連商品一覧</h4>
+    <?php do_shortcode('[product category="mold" count="4" orderby="rand" layout="column"]'); ?>
+  </div>
+  <div class="articles">
+    <h4>試作・型材料の記事</h4>
+    <?php do_shortcode('[post category="mold" count="6" orderby="rand" layout="column"]'); ?>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-related -->
