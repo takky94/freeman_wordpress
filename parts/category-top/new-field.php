425983f87bed62a409fde7434b9cc3385a577d2d
takky94 <infourbh@gmail.com>
2021-05-23 18:32:46 +0900

    新たな取り組みカテゴリページ

--- parts/category-top/new-field.php
+++ parts/category-top/new-field.php
@@ -13,3 +13,171 @@
   </div>
 </div>
 <!-- // category-lead -->
+<!-- category-children -->
+<div class="category-children left-text">
+  <div class="category-children__top">
+    <div class="category-children__top--text">
+      <h3 class="label">工業用消臭剤</h3>
+      <p class="title">化学反応で瞬間脱臭<br />違いを感じてください</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/new-field/children-1.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">INDUSTRIAL DEODORANT</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children right-text">
+  <div class="category-children__top">
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/new-field/children-2.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+    <div class="category-children__top--text">
+      <h3 class="label">ミネラルキャスティング</h3>
+      <p class="title">高い振動減衰率と優れた熱安定性を有する、次世代エンジニアリング素材</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">MINERAL CASTING</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children left-text">
+  <div class="category-children__top">
+    <div class="category-children__top--text">
+      <h3 class="label">CO2クリーニングシステム</h3>
+      <p class="title">Dry, Green, Claen</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/new-field/children-3.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">CO2 CLEANING SYSTEM</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children right-text">
+  <div class="category-children__top">
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/new-field/children-4.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+    <div class="category-children__top--text">
+      <h3 class="label">プロ用ホットメルトシステム</h3>
+      <p class="title">ホットメルト輸入販売第一人者の誇り</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">HOTMELT SYSTEM</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children left-text">
+  <div class="category-children__top">
+    <div class="category-children__top--text">
+      <h3 class="label">軸受けセンシングシステム</h3>
+      <p class="title">不足の事態を予測する。</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/new-field/children-5.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">BEARING SENSING SYSTEM</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-children -->
+<div class="category-children right-text">
+  <div class="category-children__top">
+    <div class="category-children__top--image">
+      <img src="<?= get_template_directory_uri(); ?>/images/category/new-field/children-6.png" alt=""
+        <?php fm_lazyload(); ?> />
+    </div>
+    <div class="category-children__top--text">
+      <h3 class="label">暑さ対策品</h3>
+      <p class="title">"PUT IN PUT OUT"<br />工場や施設内をクールダウンし快適な環境へ</p>
+      <p
+        class="description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+  </div>
+  <div class="category-children__bottom">
+    <p class="category-children__bottom--category">PUT IN PUT OUT</p>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-children -->
+<!-- category-related -->
+<div class="category-related">
+  <div class="products">
+    <h4>試作・型材料の関連商品一覧</h4>
+    <?php do_shortcode('[product category="mold" count="4" orderby="rand" layout="column"]'); ?>
+  </div>
+  <div class="articles">
+    <h4>試作・型材料の記事</h4>
+    <?php do_shortcode('[post category="mold" count="6" orderby="rand" layout="column"]'); ?>
+    <div class="view-all">
+      <a href="#" class="button-arrow button-line arrow-wrap">
+        <span class="font-robot bold">SEE MORE</span>
+        <?php get_template_part('/parts/icon/arrow'); ?>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- // category-related -->
