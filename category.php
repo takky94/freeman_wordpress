3e48e9a06d5f59f3e2c94c658fd184b51534b9c5
takky94 <infourbh@gmail.com>
2021-05-24 12:02:36 +0900

    親階層かどうか

--- category.php
+++ category.php
@@ -4,13 +4,14 @@
   $category = $category[0];
   $category_name = $category -> cat_name; // カテゴリー名
   $category_slug = $category -> category_nicename; // カテゴリのスラッグ
+  $is_parent = $category -> category_parent == 0 ? true : false; // 第一階層か
 ?>
 <?php get_header(); ?>
 <!--カテゴリページ-->
 <div id="category">
   <main id="main" class="main <?= $category_slug; ?>" role="main">
     <article id="article" <?php post_class(); ?>>
-      <?php get_template_part('/parts/header/category-header'); ?>
+      <?php $is_parent ? get_template_part('/parts/header/category-header') : get_template_part('/parts/header/category-children-header'); ?>
       <div id="content">
         <div class="container">
           <?php get_template_part('/parts/category-top/'.$category_slug); ?>
