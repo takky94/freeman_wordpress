b169f82daae4b3bdcd5317db6e79752c99746536
takky94 <infourbh@gmail.com>
2021-05-29 10:33:43 +0900

    ヒアドキュメントのインデント削除(あるとエラー出る可能性あり)

--- library/functions/custom-fields.php
+++ library/functions/custom-fields.php
@@ -41,137 +41,137 @@ if (!function_exists('fm_create_category_child_custom_fields')){
     $admin_url = admin_url();
 
     echo <<<EOM
-      <!-- ジュエリー子カテゴリ用カスタムフィールド -->
-      <tr class="form-field parent-jewelry">
-        <th><label for="jewelryText">リード文</label></th>
-        <td>
-          <textarea name="category_meta[jewelry_text]" id="jewelryText" rows="5" cols="40">$jewelry_text</textarea>
-          <p class="description">カテゴリページの冒頭で画像とともに表示するテキストです。第一階層のカテゴリ(型製品、砂型鋳造...etc)では表示されず、第二階層のカテゴリページで表示されます。</p>
-        </td>
-      </tr>
-      <tr class="form-field parent-jewelry">
-        <th><label for="jewelryImage">リード画像 URL</label></th>
-        <td>
-          <div style="display:flex">
-            <input id="jewelryImage" class="js-uploadImage" type="text" size="36" name="category_meta[jewelry_img]" value="$jewelry_img" />
-            <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
-          </div>
-          <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$jewelry_img" style="width: 150px; height: auto;"></p>
-        </td>
-      </tr>
-      <tr class="form-field parent-jewelry">
-        <th><label for="jewelryTable1">表1</label></th>
-        <td>
-          <input type="text" name="category_meta[jewelry_table1]" id="jewelryTable1" value="$jewelry_table1" placeholder="[table id=1]" />
-          <p class="description">画像とテキストの下の表部分です。<a href="$admin_url/admin.php?page=tablepress">TablePressというプラグイン</a>から編集できます。この欄にはショートコードを入力してください。</p>
-          <p>例: <pre><code>[table id=1]</code></pre></p>
-        </td>
-      </tr>
-      <tr class="form-field parent-jewelry">
-        <th><label for="jewelryTable2">表2</label></th>
-        <td>
-          <input type="text" name="category_meta[jewelry_table2]" id="jewelryTable2" value="$jewelry_table2" placeholder="[table id=2]" />
-        </td>
-      </tr>
-      <tr class="form-field parent-jewelry">
-        <th><label for="jewelryTable3">表3</label></th>
-        <td>
-          <input type="text" name="category_meta[jewelry_table3]" id="jewelryTable3" value="$jewelry_table3" placeholder="[table id=3]" />
-        </td>
-      </tr>
-      <tr class="form-field parent-jewelry">
-        <th><label for="jewelryTable4">表4</label></th>
-        <td>
-          <input type="text" name="category_meta[jewelry_table4]" id="jewelryTable4" value="$jewelry_table4" placeholder="[table id=4]" />
-        </td>
-      </tr>
-      <tr class="form-field parent-jewelry">
-        <th><label for="jewelryTable5">表5</label></th>
-        <td>
-          <input type="text" name="category_meta[jewelry_table5]" id="jewelryTable5" value="$jewelry_table5" placeholder="[table id=5]" />
-        </td>
-      </tr>
-      <!-- その他子カテゴリ用カスタムフィールド -->
-      <tr class="form-field parent-others">
-        <th><label for="othersImage">スライダー横画像 URL</label></th>
-        <td>
-          <div style="display:flex">
-            <input id="othersImage" class="js-uploadImage" type="text" size="36" name="category_meta[others_img]" value="$others_img" placeholder="https://..." />
-            <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
-          </div>
-          <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_img" style="width: 150px; height: auto;"></p>
-        </td>
-      </tr>
-      <tr class="form-field parent-others">
-        <th><label for="othersSliderImage1">スライダー画像1 URL</label></th>
-        <td>
-          <div style="display:flex">
-            <input id="othersSliderImage1" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img1]" value="$others_slider_img1" placeholder="https://..." />
-            <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
-          </div>
-          <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img1" style="width: 150px; height: auto;"></p>
-        </td>
-      </tr>
-      <tr class="form-field parent-others">
-        <th><label for="othersSliderImage2">スライダー画像2 URL</label></th>
-        <td>
-          <div style="display:flex">
-            <input id="othersSliderImage2" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img2]" value="$others_slider_img2" placeholder="https://..." />
-            <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
-          </div>
-          <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img2" style="width: 150px; height: auto;"></p>
-        </td>
-      </tr>
-      <tr class="form-field parent-others">
-        <th><label for="othersSliderImage3">スライダー画像3 URL</label></th>
-        <td>
-          <div style="display:flex">
-            <input id="othersSliderImage3" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img3]" value="$others_slider_img3" placeholder="https://..." />
-            <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
-          </div>
-          <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img3" style="width: 150px; height: auto;"></p>
-        </td>
-      </tr>
-      <tr class="form-field parent-others">
-        <th><label for="othersSliderImage4">スライダー画像4 URL</label></th>
-        <td>
-          <div style="display:flex">
-            <input id="othersSliderImage4" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img4]" value="$others_slider_img4" placeholder="https://..." />
-            <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
-          </div>
-          <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img4" style="width: 150px; height: auto;"></p>
-        </td>
-      </tr>
-      <tr class="form-field parent-others">
-        <th><label for="othersSliderImage5">スライダー画像5 URL</label></th>
-        <td>
-          <div style="display:flex">
-            <input id="othersSliderImage5" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img5]" value="$others_slider_img5" placeholder="https://..." />
-            <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
-          </div>
-          <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img5" style="width: 150px; height: auto;"></p>
-        </td>
-      </tr>
-      <tr class="form-field parent-others">
-        <th><label for="othersLeadTitle">リード文タイトル</label></th>
-        <td>
-          <input id="othersLeadTitle" type="text" size="36" name="category_meta[others_lead_title]" value="$others_lead_title" />
-        </td>
-      </tr>
-      <tr class="form-field parent-others">
-        <th><label for="othersLeadText">リード文</label></th>
-        <td>
-          <textarea name="category_meta[others_lead_text]" id="othersLeadText" rows="5" cols="40">$others_lead_text</textarea>
-          <p class="description">カテゴリページの冒頭で画像とともに表示するテキストです。第一階層のカテゴリ(型製品、砂型鋳造...etc)では表示されず、第二階層のカテゴリページで表示されます。</p>
-        </td>
-      </tr>
-      <tr class="form-field parent-others">
-        <th><label for="othersYouTube">YouTubeのURL</label></th>
-        <td>
-          <input id="othersYouTube" type="text" size="36" name="category_meta[others_youtube]" value="$others_youtube" placeholder="https://youtube.com/watch?v=XXXXXX" />
-        </td>
-      </tr>
-    EOM;
+<!-- ジュエリー子カテゴリ用カスタムフィールド -->
+<tr class="form-field parent-jewelry">
+  <th><label for="jewelryText">リード文</label></th>
+  <td>
+    <textarea name="category_meta[jewelry_text]" id="jewelryText" rows="5" cols="40">$jewelry_text</textarea>
+    <p class="description">カテゴリページの冒頭で画像とともに表示するテキストです。第一階層のカテゴリ(型製品、砂型鋳造...etc)では表示されず、第二階層のカテゴリページで表示されます。</p>
+  </td>
+</tr>
+<tr class="form-field parent-jewelry">
+  <th><label for="jewelryImage">リード画像 URL</label></th>
+  <td>
+    <div style="display:flex">
+      <input id="jewelryImage" class="js-uploadImage" type="text" size="36" name="category_meta[jewelry_img]" value="$jewelry_img" />
+      <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
+    </div>
+    <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$jewelry_img" style="width: 150px; height: auto;"></p>
+  </td>
+</tr>
+<tr class="form-field parent-jewelry">
+  <th><label for="jewelryTable1">表1</label></th>
+  <td>
+    <input type="text" name="category_meta[jewelry_table1]" id="jewelryTable1" value="$jewelry_table1" placeholder="[table id=1]" />
+    <p class="description">画像とテキストの下の表部分です。<a href="$admin_url/admin.php?page=tablepress">TablePressというプラグイン</a>から編集できます。この欄にはショートコードを入力してください。</p>
+    <p>例: <pre><code>[table id=1]</code></pre></p>
+  </td>
+</tr>
+<tr class="form-field parent-jewelry">
+  <th><label for="jewelryTable2">表2</label></th>
+  <td>
+    <input type="text" name="category_meta[jewelry_table2]" id="jewelryTable2" value="$jewelry_table2" placeholder="[table id=2]" />
+  </td>
+</tr>
+<tr class="form-field parent-jewelry">
+  <th><label for="jewelryTable3">表3</label></th>
+  <td>
+    <input type="text" name="category_meta[jewelry_table3]" id="jewelryTable3" value="$jewelry_table3" placeholder="[table id=3]" />
+  </td>
+</tr>
+<tr class="form-field parent-jewelry">
+  <th><label for="jewelryTable4">表4</label></th>
+  <td>
+    <input type="text" name="category_meta[jewelry_table4]" id="jewelryTable4" value="$jewelry_table4" placeholder="[table id=4]" />
+  </td>
+</tr>
+<tr class="form-field parent-jewelry">
+  <th><label for="jewelryTable5">表5</label></th>
+  <td>
+    <input type="text" name="category_meta[jewelry_table5]" id="jewelryTable5" value="$jewelry_table5" placeholder="[table id=5]" />
+  </td>
+</tr>
+<!-- その他子カテゴリ用カスタムフィールド -->
+<tr class="form-field parent-others">
+  <th><label for="othersImage">スライダー横画像 URL</label></th>
+  <td>
+    <div style="display:flex">
+      <input id="othersImage" class="js-uploadImage" type="text" size="36" name="category_meta[others_img]" value="$others_img" placeholder="https://..." />
+      <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
+    </div>
+    <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_img" style="width: 150px; height: auto;"></p>
+  </td>
+</tr>
+<tr class="form-field parent-others">
+  <th><label for="othersSliderImage1">スライダー画像1 URL</label></th>
+  <td>
+    <div style="display:flex">
+      <input id="othersSliderImage1" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img1]" value="$others_slider_img1" placeholder="https://..." />
+      <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
+    </div>
+    <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img1" style="width: 150px; height: auto;"></p>
+  </td>
+</tr>
+<tr class="form-field parent-others">
+  <th><label for="othersSliderImage2">スライダー画像2 URL</label></th>
+  <td>
+    <div style="display:flex">
+      <input id="othersSliderImage2" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img2]" value="$others_slider_img2" placeholder="https://..." />
+      <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
+    </div>
+    <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img2" style="width: 150px; height: auto;"></p>
+  </td>
+</tr>
+<tr class="form-field parent-others">
+  <th><label for="othersSliderImage3">スライダー画像3 URL</label></th>
+  <td>
+    <div style="display:flex">
+      <input id="othersSliderImage3" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img3]" value="$others_slider_img3" placeholder="https://..." />
+      <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
+    </div>
+    <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img3" style="width: 150px; height: auto;"></p>
+  </td>
+</tr>
+<tr class="form-field parent-others">
+  <th><label for="othersSliderImage4">スライダー画像4 URL</label></th>
+  <td>
+    <div style="display:flex">
+      <input id="othersSliderImage4" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img4]" value="$others_slider_img4" placeholder="https://..." />
+      <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
+    </div>
+    <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img4" style="width: 150px; height: auto;"></p>
+  </td>
+</tr>
+<tr class="form-field parent-others">
+  <th><label for="othersSliderImage5">スライダー画像5 URL</label></th>
+  <td>
+    <div style="display:flex">
+      <input id="othersSliderImage5" class="js-uploadImage" type="text" size="36" name="category_meta[others_slider_img5]" value="$others_slider_img5" placeholder="https://..." />
+      <button type="button" class="js-uploadImageButton button button-primary">画像をアップロード</button>
+    </div>
+    <p style="margin-top: 7px;text-align:center"><img class="js-uploadImageDemo" src="$others_slider_img5" style="width: 150px; height: auto;"></p>
+  </td>
+</tr>
+<tr class="form-field parent-others">
+  <th><label for="othersLeadTitle">リード文タイトル</label></th>
+  <td>
+    <input id="othersLeadTitle" type="text" size="36" name="category_meta[others_lead_title]" value="$others_lead_title" />
+  </td>
+</tr>
+<tr class="form-field parent-others">
+  <th><label for="othersLeadText">リード文</label></th>
+  <td>
+    <textarea name="category_meta[others_lead_text]" id="othersLeadText" rows="5" cols="40">$others_lead_text</textarea>
+    <p class="description">カテゴリページの冒頭で画像とともに表示するテキストです。第一階層のカテゴリ(型製品、砂型鋳造...etc)では表示されず、第二階層のカテゴリページで表示されます。</p>
+  </td>
+</tr>
+<tr class="form-field parent-others">
+  <th><label for="othersYouTube">YouTubeのURL</label></th>
+  <td>
+    <input id="othersYouTube" type="text" size="36" name="category_meta[others_youtube]" value="$others_youtube" placeholder="https://youtube.com/watch?v=XXXXXX" />
+  </td>
+</tr>
+EOM;
   }
 }
 
