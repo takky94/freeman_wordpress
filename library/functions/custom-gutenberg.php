87b8bf772dde8239fa760f48af0d4ac0a2fdda4e
takky94 <infourbh@gmail.com>
2021-06-09 18:57:29 +0900

    /single Gutenberg用スタイル追加

--- library/functions/custom-gutenberg.php
+++ library/functions/custom-gutenberg.php
@@ -9,46 +9,53 @@ add_action( 'init', 'fm_custom_gutenberg' );
 if (!function_exists('fm_custom_gutenberg')){
   function fm_custom_gutenberg(){
     register_block_style(
-        'core/heading',
-        array(
-            'name'         => 'design01',
-            'label'        => 'デザイン01',
-            'inline_style' => '.is-style-design01 {
-                border-left: solid 8px orange;
-                padding-left: 12px;
-            }',
-        )
+      'core/table',
+      array(
+        'name' => 'fm_table_details',
+        'label' => '製品詳細',
+        'inline_style' => '
+        .is-style-fm_table_details td{
+          border-left: none;
+          border-right: none;
+          padding: 2em;
+        }
+        .is-style-fm_table_details tr td:nth-child(1){
+          background: #f5f5f5;
+          color: #0e0e0e;
+          text-align: center;
+          max-width: 15%;
+        }
+        .is-style-fm_table_details tr td:nth-child(2){
+          font-size: 14px;
+        }
+        ',
+      )
     );
     register_block_style(
-        'core/table',
-        array(
-            'name'         => 'fm_table_details',
-            'label'        => '製品詳細',
-            'inline_style' => '
-            .is-style-fm_table_details tr td:nth-child(1){
-              background: #f5f5f5;
-              color: #0e0e0e;
-              width: 20%;
-            }
-            .is-style-fm_table_details td{
-              border-left: none;
-              border-right: none;
-              padding: 2em 3em;
-            }
-            ',
-        )
-    );
-    register_block_style(
-        'core/table',
-        array(
-            'name'         => 'fm_table_physical_property',
-            'label'        => '標準物性',
-            'inline_style' => '
-            .is-style-fm_table_physical_property thead th{
-              background: #888;
-              color: #fff;
-            }',
-        )
+      'core/table',
+      array(
+        'name' => 'fm_table_physical_property',
+        'label' => '標準物性',
+        'inline_style' => '
+        .is-style-fm_table_physical_property thead th{
+          background: #888;
+          color: #fff;
+          font-size: 20px;
+          text-align: left;
+          padding: 0.5em 1.5em;
+        }
+        .is-style-fm_table_physical_property tr td:nth-child(1){
+          background: #f5f5f5;
+          color: #0e0e0e;
+          width: 20%;
+        }
+        .is-style-fm_table_physical_property td{
+          border-left: none;
+          border-right: none;
+          padding: 2em 3em;
+        }
+        ',
+      )
     );
   }
 }
