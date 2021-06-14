cbf5f2aee69b5efb05afdee02dfcf2754662e36c
takky94 <infourbh@gmail.com>
2021-06-14 12:31:17 +0900

    /wp-admin カテゴリに関するバリデーション追加

--- js/backend/post-validation.js
+++ js/backend/post-validation.js
@@ -33,10 +33,19 @@ window.document.addEventListener("DOMContentLoaded", function () {
         });
         // 保存をロックして公開出来ないように
         dispatch("core/editor").lockPostSaving("fm_category_lock");
-        // カテゴリ選択
+        // カテゴリ複数選択
+      } else if (categories.length > 1) {
+        dispatch("core/notices").createNotice("error", "カテゴリーの選択は一つまで可能です", {
+          id: "fm_notice_categories",
+          isDismissible: false,
+        });
+        // 保存をロックして公開出来ないように
+        dispatch("core/editor").lockPostSaving("fm_category_lock");
+        // カテゴリ選択済
       } else {
         // 通知を非表示にして保存のロックを解除
         dispatch("core/notices").removeNotice("fm_notice_category");
+        dispatch("core/notices").removeNotice("fm_notice_categories");
         dispatch("core/editor").unlockPostSaving("fm_category_lock");
       }
     };
@@ -46,6 +55,8 @@ window.document.addEventListener("DOMContentLoaded", function () {
     // 変更ごとに実行
     subscribe(() => {
       const newCategories = getCategories();
+      console.log(categories);
+      console.log(newCategories);
       const categoriesChanged = newCategories !== categories;
       categories = newCategories;
       if (!categoriesChanged) return;
