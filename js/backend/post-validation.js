6da31ed6ea58692f78b396bd8539914915ef01eb
takky94 <infourbh@gmail.com>
2021-06-16 14:31:00 +0900

    /wp-admin バリデーション用JS一部削除

--- js/backend/post-validation.js
+++ js/backend/post-validation.js
@@ -19,7 +19,8 @@ window.document.addEventListener("DOMContentLoaded", function () {
 
   // エディタが読み込まれたら実行
   isEditorReadyPromise.then(() => {
-    const getCategories = () => select("core/editor").getEditedPostAttribute("categories");
+    const getCategories = () =>
+      select("core/editor").getEditedPostAttribute("categories");
     // 編集画面を開いた時点での選択カテゴリ
     let categories = getCategories();
 
@@ -27,25 +28,29 @@ window.document.addEventListener("DOMContentLoaded", function () {
     const switchAlert = () => {
       // カテゴリ未選択
       if (categories.length === 0) {
-        dispatch("core/notices").createNotice("error", "カテゴリーを選択してください", {
-          id: "fm_notice_category",
-          isDismissible: false,
-        });
+        dispatch("core/notices").createNotice(
+          "error",
+          "カテゴリーを選択してください",
+          {
+            id: "fm_notice_category",
+            isDismissible: false,
+          }
+        );
         // 保存をロックして公開出来ないように
         dispatch("core/editor").lockPostSaving("fm_category_lock");
         // カテゴリ複数選択
-      } else if (categories.length > 1) {
-        dispatch("core/notices").createNotice("error", "カテゴリーの選択は一つまで可能です", {
-          id: "fm_notice_categories",
-          isDismissible: false,
-        });
-        // 保存をロックして公開出来ないように
-        dispatch("core/editor").lockPostSaving("fm_category_lock");
+        // } else if (categories.length > 1) {
+        //   dispatch("core/notices").createNotice("error", "カテゴリーの選択は一つまで可能です", {
+        //     id: "fm_notice_categories",
+        //     isDismissible: false,
+        //   });
+        //   // 保存をロックして公開出来ないように
+        //   dispatch("core/editor").lockPostSaving("fm_category_lock");
         // カテゴリ選択済
       } else {
         // 通知を非表示にして保存のロックを解除
         dispatch("core/notices").removeNotice("fm_notice_category");
-        dispatch("core/notices").removeNotice("fm_notice_categories");
+        // dispatch("core/notices").removeNotice("fm_notice_categories");
         dispatch("core/editor").unlockPostSaving("fm_category_lock");
       }
     };
@@ -55,8 +60,6 @@ window.document.addEventListener("DOMContentLoaded", function () {
     // 変更ごとに実行
     subscribe(() => {
       const newCategories = getCategories();
-      console.log(categories);
-      console.log(newCategories);
       const categoriesChanged = newCategories !== categories;
       categories = newCategories;
       if (!categoriesChanged) return;
