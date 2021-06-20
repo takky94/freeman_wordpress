aaaf4c7ccab85fe6e818e387757a60db61fc04a2
takky94 <infourbh@gmail.com>
2021-06-20 15:44:29 +0900

    /category  ジュエリー以外の子階層修正

--- parts/category-top/children/others.php
+++ parts/category-top/children/others.php
@@ -16,8 +16,8 @@
   array_push($others_slider_imgs, $others_slider_img1, $others_slider_img2, $others_slider_img3 , $others_slider_img4, $others_slider_img5);
 
   // 説明文
-  $others_description_title = $contents['others_description_title'] ? esc_html($contents['others_description_title']) : '<p style="color:tomato">カテゴリページにて「説明文タイトル」を設定してください</p>';
-  $others_description_text = $contents['others_description_text'] ? esc_html($contents['others_description_text']) : '<p style="color:tomato">カテゴリページにて「説明文」を設定してください</p>';
+  $others_description_title = $contents['others_description_title'] ? esc_html($contents['others_description_title']) : '<p class="error-message">カテゴリページにて「説明文タイトル」を設定してください</p>';
+  $others_description_text = $contents['others_description_text'] ? esc_html($contents['others_description_text']) : '<p class="error-message">カテゴリページにて「説明文」を設定してください</p>';
 
   // YouTube
    $others_youtube = $contents['others_youtube']
